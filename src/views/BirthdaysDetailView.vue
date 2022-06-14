28918f3cb33d8094fa4b30da60bc34016d23f119
Emmett Walsh <emmettwalsh@gmail.com>
2022-06-15 00:16:07 +0100

    infra

--- src/views/BirthdaysDetailView.vue
+++ src/views/BirthdaysDetailView.vue
@@ -4,6 +4,6 @@ import TheWelcome from '@/components/TheWelcome.vue'
 
 <template>
   <main>
-    Home
+    Birthday detail view..
   </main>
 </template>
--- src/views/BirthdaysMasterView.vue
+++ src/views/BirthdaysMasterView.vue
@@ -4,6 +4,6 @@ import TheWelcome from '@/components/TheWelcome.vue'
 
 <template>
   <main>
-    Home
+    Birthdays master view..
   </main>
 </template>
